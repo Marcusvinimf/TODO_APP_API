8d99bf3a6eca5fa63c95fce0379609b45cd0c5bf
Marcusvinimf <marcus_v_ina@hotmail.com>
2021-01-18 16:12:14 -0300

    modificação finais

--- src/controllers/tarefa_controllers.js
+++ src/controllers/tarefa_controllers.js
@@ -1,10 +1,12 @@
 const tar = 'tarefas';
 
-// verbo get 
-module.exports = (app)=>{ app.get(`/${tar}`, (req, res) => {
+module.exports = (app)=>{ 
+        
+        app.get(`/${tar}`, (req, res) => {
                 res.send(`Rota ativada com GET e recurso ${tar}: valores de ${tar} devem ser retornados`)
-        });
-                app.post(`/${tar}`, (req, res) => {
+        })
+
+        app.post(`/${tar}`, (req, res) => {
                 res.send(`Rota POST de ${tar} ativada: ${tar} adicionado ao banco de dados`)
         })
 };
