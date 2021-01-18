6e475386a1067821ec07db3b23e04b22051df6ce
Marcusvinimf <marcus_v_ina@hotmail.com>
2021-01-18 16:14:09 -0300

    modificacao final

--- src/controllers/usuario_controllers.js
+++ src/controllers/usuario_controllers.js
@@ -1,10 +1,11 @@
 const usu = 'usuarios';
 
-// verbo get 
-module.exports = (app)=>{ app.get(`/${usu}`, (req, res) => {
+module.exports = (app)=>{ 
+        app.get(`/${usu}`, (req, res) => {
                 res.send(`Rota ativada com GET e recurso ${usu}: valores de ${usu} devem ser retornados`)
-        });
-                app.post(`/${usu}`, (req, res) => {
+        })
+        
+        app.post(`/${usu}`, (req, res) => {
                 res.send(`Rota POST de ${usu} ativada: ${usu} adicionado ao banco de dados`)
         })
 };
