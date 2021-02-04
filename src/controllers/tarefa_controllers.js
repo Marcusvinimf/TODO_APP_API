a6c293ca47b071cce3e12da1a2209b36b2843320
Marcusvinimf <marcus_v_ina@hotmail.com>
2021-02-04 12:12:40 -0300

    ultimas modificações

--- src/controllers/tarefa_controllers.js
+++ src/controllers/tarefa_controllers.js
@@ -6,76 +6,56 @@ module.exports = (app, bd) =>
 {
 	const tarefasDao = new TarefasDao(bd);
 
-	app.get(`/${tar}`, (req, res) => 
+	app.get(`/${tar}`, async (req, res) => 
 	{
-		listaTarefs();
-		async function listaTarefs()
-		{
-			try{
-				const tarefassRetorno = await tarefasDao.listaTarefas();
-				res.send(tarefassRetorno);
-			}catch{
-				res.send(erro);
-			}
+		try{
+			const tarefassRetorno = await tarefasDao.listaTarefas();
+			res.send(tarefassRetorno);
+		}catch(erro){
+			res.send(erro);
 		}
 	});
 
-	app.get(`/${tar}/:id`, (req, res) => 
+	app.get(`/${tar}/:id`, async (req, res) => 
 	{
-		unicoTarefs();
-		async function unicoTarefs()
-		{
-			try{
-				const tarefassRetorno = await tarefasDao.buscaUnicaTarefas(req.params.id);
-				res.status(200).send(tarefassRetorno);
-			}catch{
-				res.send(erro);
-			}
+		try{
+			const tarefassRetorno = await tarefasDao.buscaUnicaTarefas(req.params.id);
+			res.status(200).send(tarefassRetorno);
+		}catch(erro){
+			res.send(erro);
 		}
 	});
 
-	app.post(`/${tar}`, (req, res) => 
+	app.post(`/${tar}`, async (req, res) => 
 	{
-		addTarefs();
-		async function addTarefs()
-		{
-			try{
-				const tarefassRetorno = await tarefasDao.adicionaTarefas([req.body.titulo, req.body.descricao, req.body.status, req.body.dataCriacao, req.body.id_usuario]);
-				res.send(tarefassRetorno);
-			}catch{
-				res.send(erro);
-			}
+		try{
+			const tarefassRetorno = await tarefasDao.adicionaTarefas([req.body.titulo, req.body.descricao, req.body.status, req.body.dataCriacao, req.body.id_usuario]);
+			res.send(tarefassRetorno);
+		}catch(erro){
+			res.send(erro);
 		}
 	});
 
-	app.delete(`/${tar}/:id`, (req, res) => 
+	app.delete(`/${tar}/:id`, async (req, res) => 
 	{
-		deleteTarefs();
-		async function deleteTarefs()
-		{
-			try{
-				const tarefassRetorno = await tarefasDao.deletaTarefas(req.params.id);
-				res.send(tarefassRetorno);
-			}catch{
-				res.send(erro);
-			}
+		try{
+			const tarefassRetorno = await tarefasDao.deletaTarefas(req.params.id);
+			res.send(tarefassRetorno);
+		}catch(erro){
+			res.send(erro);
 		}
 	});
 
-	app.put(`/${tar}/:id`, (req, res) => 
+	app.put(`/${tar}/:id`, async (req, res) => 
 	{
 		const parametro = req.params.id;
 		const valuesParametro = [req.body.id, req.body.titulo, req.body.descricao, req.body.status, req.body.dataCriacao, req.body.id_usuario, req.params.id];
 
-		updateTarefs();
-		async function updateTarefs()
-		{
-			try{
-				const tarefassRetorno = await tarefasDao.atualizaTarefas(valuesParametro, parametro);
-				res.send(tarefassRetorno);
-			}catch{
-				res.send(erro);
-			}
+		try{
+			const tarefassRetorno = await tarefasDao.atualizaTarefas(valuesParametro, parametro);
+			res.send(tarefassRetorno);
+		}catch(erro){
+			res.send(erro);
 		}
 	});
 };
--- src/controllers/usuario_controllers.js
+++ src/controllers/usuario_controllers.js
@@ -6,76 +6,56 @@ module.exports = (app, bd) =>
 {
 	const usuariosDao = new UsuariosDao(bd);
 
-	app.get(`/${usu}`, (req, res) => 
+	app.get(`/${usu}`, async (req, res) => 
 	{
-		listaUser();
-		async function listaUser()
-		{
-			try{
-				const usuariosRetorno = await usuariosDao.listaUsuarios();
-				res.status(200).send(usuariosRetorno);
-			}catch{
-				res.send(erro);
-			}
+		try{
+			const usuariosRetorno = await usuariosDao.listaUsuarios();
+			res.status(200).send(usuariosRetorno);
+		}catch(erro){
+			res.send(erro);
 		}
 	});
 
-	app.get(`/${usu}/:id`, (req, res) => 
+	app.get(`/${usu}/:id`, async (req, res) => 
 	{
-		unicoUser();
-		async function unicoUser()
-		{
-			try{
-				const usuariosRetorno = await usuariosDao.buscaUnicaUsuarios(req.params.id);
-				res.status(200).send(usuariosRetorno);
-			}catch{
-				res.send(erro);
-			}
+		try{
+			const usuariosRetorno = await usuariosDao.buscaUnicaUsuarios(req.params.id);
+			res.status(200).send(usuariosRetorno);
+		}catch(erro){
+			res.send(erro);
 		}
 	});
 
-	app.post(`/${usu}`, (req, res) => 
+	app.post(`/${usu}`, async (req, res) => 
 	{
-		addUser();
-		async function addUser()
-		{
-			try{
-				const usuariosRetorno = await usuariosDao.adicionaUsuarios([req.body.nome, req.body.email, req.body.senha]);
-				res.status(200).send(usuariosRetorno);
-			}catch{
-				res.send(erro);
-			}
+		try{
+			const usuariosRetorno = await usuariosDao.adicionaUsuarios([req.body.nome, req.body.email, req.body.senha]);
+			res.status(200).send(usuariosRetorno);
+		}catch(erro){
+			res.send(erro);
 		}
 	});
 
-	app.delete(`/${usu}/:id`, (req, res) => 
+	app.delete(`/${usu}/:id`, async (req, res) => 
 	{
-		deleteUser();
-		async function deleteUser()
-		{
-			try{
-				const usuariosRetorno = await usuariosDao.deletaUsuarios(req.params.id);
-				res.status(200).send(usuariosRetorno);
-			}catch{
-				res.send(erro);
-			}
+		try{
+			const usuariosRetorno = await usuariosDao.deletaUsuarios(req.params.id);
+			res.status(200).send(usuariosRetorno);
+		}catch(erro){
+			res.send(erro);
 		}
 	});
 
-	app.put(`/${usu}/:id`, (req, res) => 
+	app.put(`/${usu}/:id`, async (req, res) => 
 	{
 		const parametro = req.params.id;
 		const valuesParametro = [req.body.id, req.body.nome, req.body.email, req.body.senha, req.params.id];
 
-		updateUser();
-		async function updateUser()
-		{
-			try{
-				const usuariosRetorno = await usuariosDao.atualizaUsuarios(valuesParametro, parametro);
-				res.status(200).send(usuariosRetorno);
-			}catch{
-				res.send(erro);
-			}
+		try{
+			const usuariosRetorno = await usuariosDao.atualizaUsuarios(valuesParametro, parametro);
+			res.status(200).send(usuariosRetorno);
+		}catch(erro){
+			res.send(erro);
 		}
 	});
 };
