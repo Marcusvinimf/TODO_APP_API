0cc4f40418581ef821c44d1dac21dff51c7c8d6c
Marcusvinimf <marcus_v_ina@hotmail.com>
2021-02-15 16:11:50 -0300

    ultimas modificações feitas

--- src/controllers/index_controllers.js
+++ src/controllers/index_controllers.js
@@ -1,6 +1,6 @@
 module.exports = (app) =>
 {
     app.get('/', (req, resp)=>{
-        resp.send('<center><h1>Agora coloque como parametro:</h1><br> <h3>Digite ==> *usuarios* ou *tarefas* no link do navegador acima, para acessar o banco de dados.</h3> <img src="http://images7.memedroid.com/images/UPLOADED176/560f05bde09fb.jpeg"></center>');
+        resp.send('<center><br>Autor: <strong>Marcus Vinicius Marinho Ferreira.</strong><h2>Agora coloque como parametro:</h2><h4>Digite ==> */usuarios* no link do navegador acima, para acessar o banco de dados de usuarios.</h4><h4>Digite ==> */tarefas* no link do navegador acima, para acessar o banco de dados de tarefas.</h4> <br><img height="500" src="http://images7.memedroid.com/images/UPLOADED176/560f05bde09fb.jpeg"></center>');
     });
 };
