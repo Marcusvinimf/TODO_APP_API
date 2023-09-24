b2b32fc3cf7c50811fadeb90025714cea4317dfd
Marcusvinimf <marcusvinimf@gmail.com>
2023-09-24 09:16:58 -0300

    alteracao em projeto final

--- src/controllers/index_controllers.js
+++ src/controllers/index_controllers.js
@@ -1,6 +1,6 @@
 module.exports = (app) =>
 {
     app.get('/', (req, resp)=>{
-        resp.send('<center><br>Autor: <strong>Marcus Vinicius Marinho Ferreira.</strong><h2>Agora coloque como parametro:</h2><h4>Clique aqui ==> <a href="https://todoappapi-project.herokuapp.com/usuarios">Banco de dados de usuarios</a>, para acessar o banco de dados de usuarios.</h4><h4>Clique aqui ==> <a href="https://todoappapi-project.herokuapp.com/tarefas">Banco de dados de tarefas</a>, para acessar o banco de dados de tarefas.</h4> <br><img height="500" src="http://images7.memedroid.com/images/UPLOADED176/560f05bde09fb.jpeg"></center>');
+        resp.send('<center><br>Autor: <strong>Marcus Vinicius Marinho Ferreira.</strong><h2>Agora coloque como parametro:</h2><h4>Clique aqui ==> <a href="http://localhost:3000/usuarios">Banco de dados de usuarios</a>, para acessar o banco de dados de usuarios.</h4><h4>Clique aqui ==> <a href="http://localhost:3000/tarefas">Banco de dados de tarefas</a>, para acessar o banco de dados de tarefas.</h4> <br><img height="500" src="http://images7.memedroid.com/images/UPLOADED176/560f05bde09fb.jpeg"></center>');
     });
 };
